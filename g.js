10e19330ac7c47cbc4a1d96967b9d466f9b7a034
374632897 <374632897@qq.com>
2016-09-22 18:20:42 +0800

    :smirk:

--- g.js
+++ g.js
@@ -2,7 +2,7 @@
 * @Author: Jiang Guoxi
 * @Date:   2016-09-22 17:59:56
 * @Last Modified by:   Jiang Guoxi
-* @Last Modified time: 2016-09-22 18:08:46
+* @Last Modified time: 2016-09-22 18:20:34
 */
 
 var fs = require('fs');
@@ -11,10 +11,27 @@ var ex = '/examples/';
 var DIR = path.join(__dirname, ex);
 
 var genli = function (link, name) {
-  return '<li><a href = "' + ex + link + '">' + name + '</li>';
+  return ('<li><a href = ".' + ex + link + '">' + name + '</li>');
 };
-// console.log(fs.)
+var genHtml = function (html) {
+  return "<!DOCTYPE html>" +
+  "<html lang='en'>" +
+  "<head>" +
+    "<meta charset='UTF-8'>" +
+    "<title>You Don't Need JavaScript</title>" +
+  "</head>" +
+  "<body>" +
+  html +
+  "</body>" +
+  "</html>";
+};
+var html = '<ul>';
 fs.readdirSync(DIR).forEach(function (item) {
-  // console.log(fs.fstatsSync(item))
-  // var
+  var file = path.join(DIR, item);
+  var basename = path.parse(file).name;
+  html += genli(item, basename);
 });
+html += '</ul>';
+var s = genHtml(html);
+fs.writeFile('./index.html', s);
+
